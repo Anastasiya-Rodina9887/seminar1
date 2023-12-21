7f9964606d5cf5729475d0f82b53e74d5273d46f
Anastasiya <ayrodin@gmail.com>
2023-12-21 15:40:06 +0700

    added re

--- Program.cs
+++ Program.cs
@@ -1,31 +1,48 @@
-﻿// Комментарий однострочный 
+﻿// // Комментарий однострочный 
 
-// Напишите программу, которая на вход
-// принимает два целых числа и проверяет,
-// является ли первое число квадратом второго.
-// a = 25, b = 5 => да
-// a = 2, b = 10 => нет
-// a = 9, b = -3 => да
-// a = -3, b = 9 => нет
+// // Напишите программу, которая на вход
+// // принимает два целых числа и проверяет,
+// // является ли первое число квадратом второго.
+// // a = 25, b = 5 => да
+// // a = 2, b = 10 => нет
+// // a = 9, b = -3 => да
+// // a = -3, b = 9 => нет
 
-// тип_данных имя_переменной = значение;
-// camelCase: firstNumber, secondNumber
-// int number = 9; number = 9;
-// int result = number * number; // 81
+// // тип_данных имя_переменной = значение;
+// // camelCase: firstNumber, secondNumber
+// // int number = 9; number = 9;
+// // int result = number * number; // 81
 
-// Console.ReadLine() - получить информацию из строчки
-Console.Write("Введите первое число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+// // Console.ReadLine() - получить информацию из строчки
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите первое число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-if (firstNumber == secondNumber * secondNumber)
+// if (firstNumber == secondNumber * secondNumber)
+// {
+// Console.WriteLine("Да, " + firstNumber + " является кв. от числа " + secondNumber);
+// }
+// else
+// {
+//     Console.WriteLine("НЕТ, " + firstNumber + " НЕ является кв. от числа " + secondNumber);
+// }
+
+// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// Примеры
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 => -2, -1, 0, 1, 2
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int negativeN = N * -1;
+
+while(negativeN <= N) // -3 -2 -1 0 1 2 3 
 {
-Console.WriteLine("Да, " + firstNumber + "является кв. от числа " + secondNumber);
-}
-else
-{
-    Console.WriteLine("НЕТ, " + firstNumber + " НЕ является кв. от числа " + secondNumber);
+    Console.Write(negativeN + " ");
+    negativeN++; 
 }
 
